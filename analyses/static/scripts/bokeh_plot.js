46f1d1a13cb85e935f4e9aa9435d694c2031a0f1
ajoxe <32021190+ajoxe@users.noreply.github.com>
2022-05-28 15:26:24 -0700

    Refactor bokeh plot - 1

---
+++ analyses/static/scripts/bokeh_plot.js
@@ -0,0 +1,50 @@
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+(function() {
+            var fn = function() {
+              Bokeh.safely(function() {
+                (function(root) {
+                  function embed_document(root) {
+                    var fileContent = readTextFile("/static/json/bokehplot.json");
+                    var docs_json = JSON.parse(fileContent);
+                    var render_items = [{"docid":"bf865df3-9a9c-41a8-a5e4-20d0b98404c4","roots":{"1287":"c07bff17-4985-492c-b39f-9dadaa02f9fa"}}];
+                    root.Bokeh.embed.embed_items(docs_json, render_items);
+                  }
+                  if (root.Bokeh !== undefined) {
+                    embed_document(root);
+                  } else {
+                    var attempts = 0;
+                    var timer = setInterval(function(root) {
+                      if (root.Bokeh !== undefined) {
+                        embed_document(root);
+                        clearInterval(timer);
+                      }
+                      attempts++;
+                      if (attempts > 100) {
+                        console.log("Bokeh: ERROR: Unable to run BokehJS code because BokehJS library is missing");
+                        clearInterval(timer);
+                      }
+                    }, 10, root)
+                  }
+                })(window);
+              });
+            };
+            if (document.readyState != "loading") fn();
+            else document.addEventListener("DOMContentLoaded", fn);
+          })();
