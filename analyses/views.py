55c7ec9c52c1563910f7ede70a0bdc26c552947c
Graham Higgins <gwhiggins@gannett.com>
2022-05-28 21:15:13 -0400

    Reformat

--- analyses/views.py
+++ analyses/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from analyses.models import Analysis
 
+
 def analysis_index(request):
     analyses = Analysis.objects.all()
     context = {
-    'analyses': analyses
+        'analyses': analyses
     }
     return render(request, 'analysis_index.html', context)
+
 
 def analysis_detail(request, pk):
     analysis = Analysis.objects.get(pk=pk)
     context = {
-    'analysis': analysis
+        'analysis': analysis
     }
     return render(request, 'analysis_detail.html', context)
